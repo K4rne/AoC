26c90c059c323e71289b5ee77fc40e12f0e569e2
K4rne <karnedeveloppement@gmail.com>
2022-12-12 18:24:45 +0100

    Initial commit

---
+++ AoC.js
@@ -0,0 +1,15 @@
+"use strict";
+const fs = require("fs");
+
+const year = process.argv[2].padStart(4, "0");
+const day = process.argv[3].padStart(2, "0");
+
+const solutionPath = `./solutions/${year}/day${day}.js`;
+const inputPath = `./inputs/${year}/day${day}.txt`;
+
+const solution = require(solutionPath);
+const input = fs.readFileSync(inputPath, {encoding:"utf8", flag:"r"});
+
+const {part1, part2} = solution(input);
+console.error("part1:", part1);
+console.error("part2:", part2);
